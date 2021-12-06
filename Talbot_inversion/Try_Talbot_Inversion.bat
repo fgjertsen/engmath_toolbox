059cb63052808f3d77e371f5e66d9c459d34f409
Frebbeb <fredrik.gjertsen@gmail.com>
2021-12-06 23:10:42 +0100

    Add test script to build and test examples

--- Talbot_inversion/Try_Talbot_Inversion.bat
+++ Talbot_inversion/Try_Talbot_Inversion.bat
@@ -1,3 +1,13 @@
-gcc talbot_inversion.c -o Talbot
-Talbot
+@echo off
+REM Build Talbot Inversion functions
+gcc -c talbot_inversion.c 
+REM Build Euler Inversion functions
+gcc -c euler_inversion.c
+
+REM Build Comparison examples
+gcc -o Talbot ilt_examples.c talbot_inversion.o euler_inversion.o
+del *.o
+
+REM Run examples
+Talbot 3
 pause
