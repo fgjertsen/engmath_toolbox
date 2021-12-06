fc709fe6599f2c722f42ce33722c3e8ec9ddfc51
Frebbeb <fredrik.gjertsen@gmail.com>
2021-12-06 23:09:53 +0100

    Add common files for Euler and Talbot, including a file to test examples

---
+++ Talbot_inversion/ilt_common.h
@@ -0,0 +1,22 @@
+
+/***************************************************************************************
+*	Inverse Laplace transforms														   *
+*	Inspired by MATLAB code by Tucker McClure                                          *
+*	Methods: 																		   *
+*		1) Talbot's method, according to Abate and Whitt (2006)                        *
+*		2) Euler's method															   *
+*                                                                                      *
+*	Fredrik Gjertsen 	<fredrik.gjertsen@gmail.com>  		                           *
+***************************************************************************************/
+
+#ifndef ILT_COMMON_H
+#define ILT_COMMON_H
+
+#include <stdlib.h>		/* Standard Library functions			*/
+#include <stdio.h>      /* Standard Library of Input and Output */
+#include <complex.h>    /* Standard Library of Complex Numbers 	*/
+#include <math.h>		/* Standard Library of Math				*/
+
+#define M		64
+
+#endif
---
+++ Talbot_inversion/ilt_examples.c
@@ -0,0 +1,65 @@
+
+/***************************************************************************************
+* Examples for inverse Laplace transforms											   *
+***************************************************************************************/
+
+#include "ilt_common.h"
+#include "euler_inversion.h"
+#include "talbot_inversion.h"
+
+// Set what Laplace transform function to invert (see list of functions below)
+static int nLaplaceTransform;
+
+// Set time vector
+static const double ti[] = {0.05, 1.05, 2.05, 3.05, 4.05};
+
+
+/***************************************************************************************
+* Laplace transform function 														   *
+***************************************************************************************/
+double complex F_s(double complex s)
+{	
+	switch (nLaplaceTransform) {
+	// Laplace transforms to invert:	
+	/* Alt 0: Ramp function, type 1		*/	case 0:		return ( 2.0/(s*s) );
+	/* Alt 1: Step function				*/	case 1:		return ( 1.0/s );
+	/* Alt 2: Ramp function, type 2		*/	case 2:		return ( 1.0/(s*s) );
+	/* Alt 3: Damped sine				*/	default:	return ( 2.0*M_PI/((s+2.0)*(s+2.0) + 4.0*M_PI*M_PI) );
+	}
+}
+
+
+/***************************************************************************************
+* Main entry point                                                                     *
+***************************************************************************************/
+int main(int argc, char** argv)
+{
+
+	// Allocate resources
+	int ni = (int)(sizeof(ti)/sizeof(ti[0]));
+	double *ft = (double*)malloc(2 * ni * sizeof(double));
+	
+	if (argc > 1) {
+		nLaplaceTransform = atoi(argv[1]);
+	} else {
+		nLaplaceTransform = 3;
+	}
+	
+	// Calculate inverse Laplace transforms
+	for (int i = 0; i < ni; i++) {
+		ft[i] 		= Talbot_inversion( F_s, ti[i] );		// Talbot inversion
+		ft[i + ni]	= Euler_inversion(  F_s, ti[i] );		// Euler inversion
+	}
+	
+	// Print results to console
+	printf("\nTime\t\tTalbot inversion\tEuler inversion\n");
+	for (int i = 0; i < ni; i++) {
+		printf("t = %.2f\tf(t) = %.3E\tf(t) = %.3E\n", ti[i], ft[i], ft[i+ni]);
+	}
+	printf("\n");
+	
+	// Free allocated memory
+	free(ft);
+
+    return 0;
+}
