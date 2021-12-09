191fc88715371cdf848247902e2008f854540554
Frebbeb <fredrik.gjertsen@gmail.com>
2021-12-09 22:36:11 +0100

    Fixes to Broydens method

--- NLEQ_Broydens_method/nleq_broyden.m
+++ NLEQ_Broydens_method/nleq_broyden.m
@@ -3,23 +3,24 @@
 %
 % DESCRIPTION:
 %   Find the solution for a nonlinear (set of) equation(s)
-%		on the form Ax = b, by iteration
+%		on the form Ax = b, by iteration, using Broydens method
 %
 % INPUT:
-%   xv0	- (double, vector) 				Initial guess for solution
-%   f   - (fhandle(double, vector))		Function to solve, f(x, par) = 0
-%	par	- (double, struct)				System specific parameters
-%	J 	- (fhandle(double, matrix)) 	Function to calculate Jacobian, df/dx.	(optional)
-%	tol	- (double, scalar)				Tolerance for iteration convergence 	(optional)
+%   xv0		- (double, vector) 				Initial guess for solution
+%   f   	- (fhandle(double, vector))		Function to solve, f(x, par) = 0
+%	par		- (double, struct)				System specific parameters
+%	J 		- (fhandle(double, matrix)) 	Function to calculate Jacobian, df/dx.	(optional)
+%	tol		- (double, scalar)				Tolerance for iteration convergence 	(optional)
+%	nItMax	- (integer, scalar)				Max number of iterations 				(optional)
 %
 % OUTPUT:
-%   xv	- (double, vector) 	Solution to equation
-% 	nIt - (integer, scalar)	Number of iterations needed to converge
+%   xm	- (double, vector) 	Solution to equation
+%			Dimension is [n, nIterations]
 %
-function [xv, nIt] = nleq_broyden(xv0, f, par, J, tol)
+function xm = nleq_broyden(xv0, f, par, J, tol, nItMax)
 
 	% Initiate function values
-	xv = x0;
+	xv = xv0;
 	n  = length(xv);
 	fr = feval(f, xv, par);
 	
@@ -29,10 +30,20 @@ function [xv, nIt] = nleq_broyden(xv0, f, par, J, tol)
 	else
 		Br = eye(n);
 	end
+	M = zeros(n);
+	
+	% Make sure tolerance is set
 	if isempty(tol)
 		tol = 1.e-10;
 	end
-	M = zeros(n);
+	
+	% Make sure max. iterations is set
+	if isempty(nItMax)
+		nItMax = 1000;
+	end
+	
+	% Allocate solutions matrix, with sufficient space
+	xm = zeros(n, nItMax+1);
 	
 	% Tuning parameter of Broydens method
 	tau = 1;
@@ -40,10 +51,13 @@ function [xv, nIt] = nleq_broyden(xv0, f, par, J, tol)
 	% Start iteration loop
 	bConverged = 0;
 	nIt = 0;
-	while ~conv
+	while ~bConverged
+		
+		% Save iterates
+		xm(:, nIt+1) = xv;
 		
 		% Check for convergence
-		if norm(fr) < tol
+		if norm(fr) < tol || nIt >= nItMax
 			bConverged = 1;
 			break;
 		end
@@ -55,7 +69,7 @@ function [xv, nIt] = nleq_broyden(xv0, f, par, J, tol)
 		% Update Jacobian approximation, using Broydens formula
 		dfr = feval(f, xv, par) - fr;
 		fr  = fr + dfr;
-		oyp = Br*y-pr;
+		oyp = Br*dfr - pr;
 		pB	= pr' * Br;
 		for i=1:n
 			for j=1:n
@@ -68,5 +82,8 @@ function [xv, nIt] = nleq_broyden(xv0, f, par, J, tol)
 		nIt = nIt + 1;
 		
 	end
+	
+	% Return the solutions from the iterations
+	xm = xm(:, 1:nIt+1);
 
 end
